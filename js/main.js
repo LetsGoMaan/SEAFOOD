ec8547728251b728b168bb2d38f89c600bd5b3a3
LetsGoMan <ilikeseik@gmail.com>
2022-01-15 18:54:39 +0300

    make first screen

---
+++ js/main.js
@@ -0,0 +1,11 @@
+$(function(){
+
+  $('.header-slider').slick({
+    dots: true,
+    prevArrow: '<button type="button" class="slick-prev"><img src="images/prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="slick-next"><img src="images/next.svg" alt=""></button>',
+    vertical: true
+
+  });
+
+});
