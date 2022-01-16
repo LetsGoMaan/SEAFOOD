001db823927d0cdd942a1057e55e55af0a8cc398
LetsGoMan <ilikeseik@gmail.com>
2022-01-16 15:16:36 +0300

    make next 3 blocks

--- js/main.js
+++ js/main.js
@@ -8,4 +8,24 @@ $(function(){
 
   });
 
+
+
+  $('.product__name').slick({
+  slidesToShow: 6,
+  slidesToScroll: 1,
+  focusOnSelect: true,
+  asNavFor: '.product__content',
+  vertical: true,
+   prevArrow: '<button type="button" class="product-prev"><img src="images/product-prev.svg" alt=""></button>',
+    nextArrow: '<button type="button" class="product-next"><img src="images/product-next.svg" alt=""></button>',
+    
+});
+$('.product__content').slick({
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  asNavFor: '.product__name',
+  fade: true,
+  arrows: false
+  
+});
 });
